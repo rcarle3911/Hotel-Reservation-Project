aba321f372c70a8353d177b34001f94dc9fbf76d
Robert Carle <rcarle3911@gmail.com>
2016-11-01 21:30:41 -0500

    app.js update

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-	res.send("Welcome to the Martian Motel Group")
-});
+app.use(express.static(__dirname + "/public"));
 
 app.listen(3000);
 console.log("Server running on port 3000");
