def9e68288bf38ffc6f032b578442b454af4918b
Robert Carle <rcarle3911@gmail.com>
2016-11-11 09:04:31 -0600

    Added momgojs/body-parser

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + "/public"));
 
