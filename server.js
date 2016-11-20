7467c09c811888d46609bd9f0048579ed70231bf
Robert Carle <rcarle3911@gmail.com>
2016-11-20 09:53:42 -0600

    Cleaned up server.js

--- server.js
+++ server.js
@@ -5,15 +5,35 @@ var session = require('express-session');
 var bodyParser = require('body-parser');
 var config = require('config.json');
 
+/** Node mailer example use
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport('smtps://motelmartian%40gmail.com:CMSC495@UMUC@smtp.gmail.com');
+var adminEmailList = 'motelmartian@gmail.com, rcarle3911@gmail.com, jordanf08@gmail.com, jwmooreiv@gmail.com, dsteele0301@gmail.com, smith.glenisha@me.com, audioinstalr@yahoo.com, blacksmith_22@yahoo.com, fish5802@gmail.com';
+
+var mailOptions = {
+	from: '"Martian Motel" <motelmartian@gmail.com>',
+	to: '';
+	cc: 'motelmartian@gmail.com',
+	subject: 'Martian Motel',
+	text: 'Server Online',
+	html: '<b>Server Online</b>'
+};
+
+transporter.sendMail(mailOptions, function(error, info) {
+	if(error) {
+		return console.log(error);
+	}
+	console.log('Message sent: ' + info.response);
+});
+**/
+
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
-app.use(session({ secret: config.secret, resave: false, saveUnitialized: true}));
+app.use(session({ secret: config.secret, resave: false, saveUninitialized: true}));
 
-
-//Routes
+// Routes
 app.use('/app', require('./controllers/app.controller'));
 app.use('/emp', require('./controllers/emp.controller'));
-
 // make '/app' default route
 app.get('/', function (req, res) {
     return res.redirect('/app');
