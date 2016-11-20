f0cb22c0879490530e06fae1d5e17346fb8434bf
Fish5802 <Fish5802@gmail.com>
2016-11-19 19:49:50 -0600

    Modals working on grid

---
+++ app/emp/pages/modal.js
@@ -0,0 +1,17 @@
+$('#modalTabs a').click(function (e) {
+	e.preventDefault();
+  
+	var url = $(this).attr("data-url");
+  	var href = this.hash;
+  	var pane = $(this);
+	
+	// ajax load from data-url
+	$(href).load(url,function(result){      
+	    pane.tab('show');
+	});
+});
+
+// load first tab content
+$('#Tab1').load($('.active a').attr("data-url"),function(result){
+  $('.active a').tab('show');
+});
