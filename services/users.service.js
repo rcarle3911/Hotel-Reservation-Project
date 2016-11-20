384028e361e158ee2915b32f1ec3d410d0856e49
Robert Carle <rcarle3911@gmail.com>
2016-11-20 14:06:56 -0600

    closes #22

---
+++ services/users.service.js
@@ -0,0 +1,83 @@
+var config = require('config.json');
+var _ = require('lodash');
+var jwt = require('jsonwebtoken');
+var bcrypt = require('bcryptjs');
+var Q = require('q');
+var mongojs = require('mongojs');
+var db = mongojs('hotel', ['users']);
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport('smtps://motelmartian%40gmail.com:CMSC495@UMUC@smtp.gmail.com');
+
+var service = {};
+
+service.create = create;
+
+//Test Code
+service.create({
+    firstName: "Martian",
+    lastName: "Group",
+    dob: new Date("12/31/1970"),
+    phone: "(999)999-9999",
+    email: "motelmartian@gmail.com",
+    address: "123 Main St, Springfield, MO, 90210",
+    permission: "customer",
+    password: "123456"
+    });
+
+module.exports = service;
+
+/**
+ * Creates a user object. First checks if a user exists, then creates a user with the given parameters
+ * @param {any} userParam - user object with email/password
+ */
+function create(userParam) {
+    var deferred = Q.defer();
+
+    //Checks if user exists
+    db.users.findOne(
+        { email: userParam.email },
+        function (err, user) {
+            if (err) deferred.reject(err.name + ': ' + err.message);
+
+            if (user) {
+                deferred.reject('The email "' + userParam.email + '" is already in use');
+            } else {
+                createUser();
+            }
+        });
+
+    /**
+     * Copies userParam without plaintext password. 
+     * Hashes the password and stores it into the password field.
+     * Inserts user into the database.
+     * Sends user email upon success.
+     */
+    function createUser() {
+        var user = _.omit(userParam, 'password');
+        user.password = bcrypt.hashSync(userParam.password, 10);        
+        user.permission = "customer";
+        db.users.insert(
+            user,
+            function (err, doc) {
+                if (err) deferred.reject(err.name + ': ' + err.message);
+                
+                //Send email confirmation
+                transporter.sendMail({
+                    from: '"Martian Motel" <motelmartian@gmail.com>',
+                    to: user.email,
+                    subject: 'Welcome to the Martian Motel Group',
+                    text: 'Your username is ' + user.email,
+                    html: '<p>Your username is <b>' + user.email + '</b></p>'
+                }, function(error, info) {
+                    if (error) {
+                        return console.log(error);
+                    }
+                    console.log('Message sent: ' + info.response);
+                });
+
+                deferred.resolve();
+            });
+    }
+
+    return deferred.promise;
+}
