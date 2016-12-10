f1a2a617812b2cc0201dceace95c590036f915d9
Fish5802 <Fish5802@gmail.com>
2016-12-10 11:00:49 -0600

    Folder Cleanup

---
+++ emp/mgrpages/controllers/mgrusers.controller.js
@@ -0,0 +1,19 @@
+//User Accounts Page get all users / Clear filter
+angular.module('app').controller('userCtrl', ['$scope', '$http','$window', function ($scope, $http, $window) {
+    $scope.orderByField = 'lastname';
+    $scope.reverseSort = false;
+
+    $scope.users = [];
+
+    if ($window.jwtToken) $http.defaults.headers.common['Authorization'] = 'Bearer ' + $window.jwtToken;
+
+    $http.get('/api/protected/users').then(function (res) {
+        $scope.users = JSON.parse(JSON.stringify(res.data));
+    });
+
+    $scope.clearFilter = function () {
+        console.log("Cleared Filter");
+        $scope.txtFilter = null;
+
+    };
+}]);
