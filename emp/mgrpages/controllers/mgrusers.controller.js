de1a57e4c0b03404d284509237990e893afdc485
Fish5802 <Fish5802@gmail.com>
2016-12-10 12:51:48 -0600

    Removed JSON.parse thing

--- emp/mgrpages/controllers/mgrusers.controller.js
+++ emp/mgrpages/controllers/mgrusers.controller.js
@@ -8,7 +8,7 @@ angular.module('app').controller('userCtrl', ['$scope', '$http','$window', funct
     if ($window.jwtToken) $http.defaults.headers.common['Authorization'] = 'Bearer ' + $window.jwtToken;
 
     $http.get('/api/protected/users').then(function (res) {
-        $scope.users = JSON.parse(JSON.stringify(res.data));
+        $scope.users = res.data;
     });
 
     $scope.clearFilter = function () {
