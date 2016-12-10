c342629ba204db568e00df6e9acce6faa8a2f73a
Fish5802 <Fish5802@gmail.com>
2016-12-10 12:22:45 -0600

    Manage Users Inital Modal functionallity

--- emp/mgrpages/controllers/mgrusers.controller.js
+++ emp/mgrpages/controllers/mgrusers.controller.js
@@ -18,3 +18,46 @@ angular.module('app').controller('userCtrl', ['$scope', '$http','$window', funct
     };
 }]);
 
+
+angular.module('app').controller("modalUser", ['$scope', '$modal', '$log',
+
+    function ($scope, $modal, $log) {
+
+        $scope.showForm = function () {
+            $scope.message = "Show Form Button Clicked";
+            console.log($scope.message);
+
+            var modalInstance = $modal.open({
+                templateUrl: '/emp/mgrpages/mgrusers.modal.html',
+                controller: ModalInstanceCtrl,
+                scope: $scope,
+                resolve: {
+                    userForm: function () {
+                        return $scope.userForm;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (selectedItem) {
+                $scope.selected = selectedItem;
+            }, function () {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
+        };
+            }]);
+
+var ModalInstanceCtrl = function ($scope, $modalInstance, userForm) {
+    $scope.form = {}
+    $scope.submitForm = function () {
+        if ($scope.form.userForm.$valid) {
+            console.log('user form is in scope');
+            $modalInstance.close('closed');
+        } else {
+            console.log('userform is not in scope');
+        }
+    };
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+};
