838922749def5ea2d89cdefbd9e87ec73847e803
Fish5802 <Fish5802@gmail.com>
2016-12-12 12:15:04 -0600

    User Changes

--- emp/mgrpages/controllers/mgrusers.controller.js
+++ emp/mgrpages/controllers/mgrusers.controller.js
@@ -9,7 +9,10 @@ angular.module('app').controller('userCtrl', ['$scope', '$http', '$window', '$mo
 
     $http.get('/api/protected/users').then(function (res) {
         $scope.users = res.data;
+        console.log("API users pull:");
+        console.log($scope.users); 
     });
+    
 
     $scope.clearFilter = function () {
         console.log("Cleared Filter");
@@ -28,6 +31,13 @@ angular.module('app').controller('userCtrl', ['$scope', '$http', '$window', '$mo
             }
         });
     };
+    // function to evaluate if a number is even
+    $scope.isEven = function(value) {
+    console.log("isEven Running: " + value);
+    console.log((parseInt(value,10) % 2 == 0) ? true : false);
+    return (parseInt(value,10) % 2 == 0) ? true : false;
+
+    };
 
 }]);
 
@@ -41,34 +51,36 @@ app.controller('ModalInstanceCtrl', function ($scope, user, $modalInstance, $htt
         $modalInstance.dismiss('cancel');
     };
 
-    $scope.delete = function () {
+    $scope.delete = function (request, response) {
         $http.delete('/api/protected/users/' + user._id, {_id: user._id})
         .then(
             function (response) {
                 // success callback
                 console.log("Delete Sucessful");
+                console.log(JSON.stringify(response));
             },
             function (response) {
                 // failure callback
                console.log("Failed to Delete");
+               console.log(JSON.stringify(response));
 
             }
         ); 
         $modalInstance.close();
     };
 
-    $scope.ok = function () {
+    $scope.ok = function (request, response) {
             $http.put('/api/protected/users/' + user._id, user)
             .then(
                 function (response) {
                     // success callback
                     console.log("Put Sucessful");
-                    console.log(respone);
+                    console.log(JSON.stringify(response));
                 },
                 function (response) {
                     // failure callback
                     console.log("Failed to Put");
-                    console.log(respone);
+                    console.log(JSON.stringify(response));
                 }
             );
         console.log("ok clikced");
