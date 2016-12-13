032e1ba58ca04b17f4ec61a706add3fe625d145a
Daniel <dsteele0301@gmail.com>
2016-12-12 23:56:33 -0500

    Improved Create Reservation functionality Reference #94

--- app/reservations/index.controller.js
+++ app/reservations/index.controller.js
@@ -5,10 +5,39 @@
         .module('app')
         .controller('Res.IndexController', Controller);
 
-    function Controller($state, ResService, FlashService, $scope) {
+    function Controller($window, ResService, UserService, FlashService, $http) {
         var vm = this;
+
+        vm.rmtypes = [];
         vm.formInfo = {}
         vm.submitForm = submitForm;
+        
+        initController();
+
+        function initController() {
+            /*
+             * Will populate reservation Name and Email with currently logged in user
+             */
+            if ($window.jwtToken) {
+                UserService.GetCurrent()
+                .then(function (user) {
+                    vm.formInfo.Name = user.firstname + " " + user.lastname;
+                    vm.formInfo.userEmail = user.email;
+                })
+                .catch(function (err) {
+                    console.log(err);
+                });
+            }
+            
+            //Populate Room Type dropdown; map Type Names to IDs
+            $http.get('/api/public/room/type')
+            .then(function (res) {
+                vm.rmtypes = res.data;
+            })
+            .catch(function (res) {
+                console.log("Failed to pull room types");
+            });
+        }
         
         function submitForm() {
             ResService.Create(vm.formInfo)
@@ -20,6 +49,5 @@
                 });
             }
         }
-
     
 })();
