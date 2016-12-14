9abce41cca04da052a40463e20ce92dd5a153c67
Fish5802 <Fish5802@gmail.com>
2016-12-13 22:18:32 -0600

    Manage Room types

--- emp/mgrpages/controllers/manager.controller.js
+++ emp/mgrpages/controllers/manager.controller.js
@@ -1,14 +1,19 @@
 //Manager Pages Tabs (on manager.html)
 angular.module('app').controller('mgrCtrl',['$scope', function ($scope) {
         $scope.tabs = [{
-            title: 'User Accounts',
+            title: 'Users',
             contenturl: '/emp/mgrpages/mgrusers.html',
             icon: 'glyphicon glyphicon-user'
         }, {
-            title: 'Room Management',
+            title: 'Rooms',
             contenturl: '/emp/mgrpages/mgrrooms.html',
             icon: 'glyphicon glyphicon-bed'
-        }, {
+        },{
+            title: 'Room Types',
+            contenturl: '/emp/mgrpages/mgrroomtypes.html',
+            icon: 'glyphicon glyphicon-list'
+        }, 
+        {
             title: 'Occupancy Report',
             contenturl: '/emp/mgrpages/mgroccrpt.html',
             icon: 'glyphicon glyphicon-list-alt'
