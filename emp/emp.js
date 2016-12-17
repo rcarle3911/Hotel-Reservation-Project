9df32b00a235b73c2d13611aa2f9e217be84eaf0
Fish5802 <Fish5802@gmail.com>
2016-12-16 22:17:24 -0600

    Hide Management tab for non-Managers

--- emp/emp.js
+++ emp/emp.js
@@ -1,57 +1,73 @@
-$(function () {
-    $.get('/emp/token', function (token) {
-        if (token) {
-            window.jwtToken = token;
-            console.log(window.jwtToken);
-        } else {
-            window.location.href = '/login';
+function getToken() {
+    $.ajax({
+        async: false,
+        type: 'GET',
+        url: '/app/token',
+        success: function (token) {
+            if (token) {
+                window.jwtToken = token;
+            } else {
+                window.location.href = '/login';
+            }
         }
     });
-});
+};
 
 //Controller Stuff
-var app = angular.module('emp', ['ultimateDataTableServices', 'ui.bootstrap', 'chart.js', 'reservations']);
-
-
+var app = angular
+    .module('emp', ['ultimateDataTableServices', 'ui.bootstrap', 'chart.js', 'reservations'])
+    .run(function ($window, $http) {
+        getToken();
+        if ($window.jwtToken) {
+            $http.defaults.headers.common['Authorization'] = 'Bearer ' + $window.jwtToken;
+        }
+    });
 
 //Employee pages tabs (on /emp/index.html)
 app.controller('EmpCtrl', ['$scope', '$http', '$window', function ($scope, $http, $window) {
 
-    // if ($window.jwtToken) $http.defaults.headers.common['Authorization'] = 'Bearer ' + $window.jwtToken;
+    $http.get('/api/protected/users/current').then(function (res) {
+        $scope.cUser = res.data;
+        var user = $scope.cUser; 
+        if(user.group == 0)
+        {
+            //kick them out.
+           return $window.location.href = '/app'; 
+        }
 
-    // $http.get('/api/protected/users/current').then(function (res) {
-    //     $scope.cUser = res.data;
-    //     console.log("success");
-    //     console.log($scope.cUser);
-    // }, function (res) {
-    //     //faliure
-    //     console.log("error");
-    //     console.log(res.data);
-    // });
+        $scope.tabs = [{
+            title: 'Reservations',
+            contenturl: '/emp/pages/reservations.html',
+            icon: 'glyphicon glyphicon-calendar',
+            usesres: 'visible',
+            disable: !(user.group > 0)
+        }, {
+            title: 'Check In',
+            contenturl: '/emp/pages/checkin.html',
+            icon: 'glyphicon glyphicon-log-in',
+            usesres: 'visible',
+            disable: !(user.group > 0)
+        }, {
+            title: 'Check-Out',
+            contenturl: '/emp/pages/checkout.html',
+            icon: 'glyphicon glyphicon-log-out',
+            usesres: 'visible',
+            disable: !(user.group > 0)
+        }, {
+            title: 'Management',
+            contenturl: '/emp/mgrpages/manager.html',
+            icon: 'glyphicon glyphicon-wrench',
+            usesres: 'hidden',
+            disable: !(user.group > 1)
+        }];
+    }, function (res) {
+        //faliure
+        console.log(res.data);
+    });
 
-    $scope.tabs = [{
-        title: 'Reservations',
-        contenturl: '/emp/pages/reservations.html',
-        icon: 'glyphicon glyphicon-calendar',
-        usesres: 'visible',
-        disable: false
-    }, {
-        title: 'Check In',
-        contenturl: '/emp/pages/checkin.html',
-        icon: 'glyphicon glyphicon-log-in',
-        usesres: 'visible',
-        disable: false
-    }, {
-        title: 'Check-Out',
-        contenturl: '/emp/pages/checkout.html',
-        icon: 'glyphicon glyphicon-log-out',
-        usesres: 'visible',
-        disable: false
-    }, {
-        title: 'Management',
-        contenturl: '/emp/mgrpages/manager.html',
-        icon: 'glyphicon glyphicon-wrench',
-        usesres: 'hidden',
-        disable: false
-    }];
+
+
+
+
+
 }]);
