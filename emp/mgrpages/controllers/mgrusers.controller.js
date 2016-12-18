a3b4e274e02c3071fbe5fdbcd23d95dea4376100
Fish5802 <Fish5802@gmail.com>
2016-12-17 20:55:25 -0600

    User Filtering Fixed

--- emp/mgrpages/controllers/mgrusers.controller.js
+++ emp/mgrpages/controllers/mgrusers.controller.js
@@ -7,29 +7,25 @@ angular.module('emp').controller('userCtrl', ['$scope', '$http', '$window', '$mo
 
     if ($window.jwtToken) $http.defaults.headers.common['Authorization'] = 'Bearer ' + $window.jwtToken;
 
-    loadUserData(); 
+    loadUserData();
 
     function loadUserData() {
         $http.get('/api/protected/users').then(function (res) {
             $scope.users = res.data;
+            angular.forEach($scope.users, function (obj) {
+                if (obj.group == 1) {
+                    obj.Membership = "Employee";
+                } else if (obj.group == 2) {
+                    obj.Membership = "Manager";
+                } else {
+                    obj.Membership = "Customer";
+                }
+            });
         });
 
     }
 
-    $scope.getUserGroup = function (_user) {
-        if(_user.group == 1)
-        {
-            return "Employee";
-        }
-        else if(_user.group == 2)
-        {
-            return "Manager";
-        }
-        else 
-        {
-            return "Customer";
-        }
-    }
+
 
     $scope.clearUserFilter = function () {
         $scope.txtUserFilter = null;
@@ -46,7 +42,7 @@ angular.module('emp').controller('userCtrl', ['$scope', '$http', '$window', '$mo
             }
         });
         modalInstance.result.then(function () {
-            $scope.users = []; 
+            $scope.users = [];
             loadUserData();
         });
     };
@@ -54,39 +50,40 @@ angular.module('emp').controller('userCtrl', ['$scope', '$http', '$window', '$mo
 
 //Mgrusers.modal.html
 app.controller('ModalInstanceCtrl', function ($scope, user, $modalInstance, $http) {
-    if(user == "new")
-    {
+    if (user == "new") {
         $scope.newUser = true;
         //temp user
         user = {
-            firstname: "", 
+            firstname: "",
             lastname: "",
             dob: "",
             phone: "",
             email: "",
-            address: "", 
-            password: "password", 
+            address: "",
+            password: "password",
             group: 0
         }
 
     }
-    
-    
+
+
     $scope.user = user;
     console.clear();
     console.log(user);
 
-    $scope.origUser = angular.copy($scope.user);  
+    $scope.origUser = angular.copy($scope.user);
 
     $scope.cancelUser = function () {
         if (!$scope.newUser) {
-        angular.copy($scope.origUser, $scope.user); 
+            angular.copy($scope.origUser, $scope.user);
         }
         $modalInstance.dismiss('cancel');
     };
-    
+
     $scope.deleteUser = function (request, response) {
-        $http.delete('/api/protected/users/' + user._id, { _id: user._id })
+        $http.delete('/api/protected/users/' + user._id, {
+                _id: user._id
+            })
             .then(
                 function (response) {
                     // success callback
@@ -96,12 +93,11 @@ app.controller('ModalInstanceCtrl', function ($scope, user, $modalInstance, $htt
                     console.log("falied to delete");
                 }
             );
-            $modalInstance.close();
+        $modalInstance.close();
     };
 
     $scope.okUser = function (request, response) {
-        if(!$scope.newUser)
-        {
+        if (!$scope.newUser) {
             $http.put('/api/protected/users/' + user._id, user)
                 .then(
                     function (response) {
